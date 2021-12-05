73a24ffad7bd5b1802ccc9a6520bf7317040839b
Tommy Catt <themeowmeowcat123@gmail.com>
2021-12-06 00:06:51 +0500

    mobile updates
    more updates lol

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ window.tmplts = {
     p_reddit: "Honestly the actual website is better imo",
     p_htmlrun: "Um you can run HTML code with this real quick :|",
     p_pagebuilder: "An HTML page builder I made, you can create beaultiful HTML pages with it",
-    p_fallingblocks: "It's really good I swear just try it:D",
+    p_fallingblocks: "It's really good I swear just try it :D",
 }
 
 function init() {
