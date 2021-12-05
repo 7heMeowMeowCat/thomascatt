fb28c2f505fddd05f89b5da6bbc57a26300ffc32
Tommy Catt <themeowmeowcat123@gmail.com>
2021-12-05 23:36:41 +0500

    Website redesign
    will probably change this later lmao

--- scripts.js
+++ scripts.js
@@ -11,4 +11,36 @@ window.tmplts = {
     p_fallingblocks: "It's really good I swear just try it:D",
 }
 
+function init() {
+    var sections = [
+        [
+            'What do I do?', 
+            '<span style="color: white; font-weight: normal;">Programming</span>, some <span style="color: white; font-weight: normal;">gaming</span> sometimes, and <span style="color: white; font-weight: normal;">anime</span> rarely, but mostly <span style="color: white; font-weight: normal;">programming</span>'
+        ],
+        [
+            'Why programming?', 
+            "Programmig is fun and I love the idea how a normal person can get started so easily if they are interested.",
+            'With the skill of programming you seem to have the sense of creating any sort of application you want, which is joyful in my case. I love bringing those thoughts to reality.</p>'
+        ],
+        [
+            'Isn\'t programming hard?', 
+            "Programmig is as simple as talking to someone.",
+            "Quite literally though.<br>The listener is the computer, and you are the speaker.<br>For interested people, programming can be really fun and also addicting.<br><br>But the point at which things seem to get a bit difficult is to keep the fact in mind that computers aren't humans, so their way of understanding is different, and perhaps too literal. Our minds are used to 'autocompleting' the every single bit of information, so, because of that, many beginners usually seem to 'assume' parts of code in their mind, with the computer utterly confused about the user's requirement.<br><br>If you're interested in getting started, I'll suggest getting started with strict languages like C#, Java, etc. (keep in mind I'm not a professional when it comes to suggestions) In my opinion, they are better for beginners in building up basic concepts more powerfully due to their strictness for them.<p>"
+        ]
+    ]
+
+    sections.forEach(a => {
+        document.querySelector('#introduction').innerHTML += '<center style="padding-top: 10%; color: #aaa; text-transform: uppercase;">'+a[0]+'</center><h1 style=" padding: 10%; padding-top: 0;"><center style="font-size: 36px; color: #eee; font-weight: 100;">'+a[1]+'<br><p style=\"font-size: 50%;\">'+(a[2] || '')+'</center></h1><br><hr><br>'
+    })
+
+
+    document.body.style = "transition-duration: 600ms;"
+    document.body.className = "b"
+    setTimeout(() => {
+        document.body.style = ""
+    }, 600);
+}
+
+init()
+
 document.querySelectorAll('.tmplt').forEach(a => a.innerHTML = window.tmplts[a.innerText])
