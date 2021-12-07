e7bdbe15cd3bbb3adf2ff9e420a4970d2052033b
Tommy Catt <themeowmeowcat123@gmail.com>
2021-12-07 22:13:51 +0500

    Diseases updates
    more update demn 🔥

--- projects/diseases_explorer/jsx/script.jsx
+++ projects/diseases_explorer/jsx/script.jsx
@@ -2,13 +2,44 @@ try {
     var request = window.request
     var testData = window.testData
     
-    function SearchDiseases() {
-        return (
-            <div className="search">
-                <input type="text" placeholder="Search the diseases list"/>
-                <i className={"fa fa-search"}></i>
-            </div>
-        )
+    class SearchDiseases extends React.Component {
+        constructor(props) {
+            super(props)
+            this.onSearchChange = this.onSearchChange.bind(this)
+        }
+
+        onSearchChange(e) {
+            clearTimeout(window.searchDelay)
+            window.searchDelay = setTimeout((function() {
+                var searchText = e.target.value // the search text
+                var searchTerms = searchText.split(" ").filter(a => a != "") // search text split on spaces to form 'terms' to match
+                if (searchTerms.length > 0) { // only continue search if the search terms exist
+                    var searchedItemsRelevance = [] // relevance array, stores relevance for each match, used for sorting later
+                    var searchedItems = this.props.state.diseases.filter(function (a) { // performs the actual matching
+                        var matches = searchTerms.map(t => a.tags.map(tag => tag.includes(t)).reduce( (a, b) => a || b)) // check for tags of each disease, and run a .includes() for each of the search terms
+                        searchedItemsRelevance.push(matches.filter(a => a == true).length) // checks relevance, which is number of search terms found in tags
+                        return matches.reduce( (a, b) => a || b)
+                    })
+                    
+                    for (var i = 0; i < searchedItems.length; i++) { // attach the separated search relevance to the searchedItems array
+                        searchedItems[i]._relevance = searchedItemsRelevance[i]
+                    }
+    
+                    searchedItems = searchedItems.sort( (a, b) => a._relevance > b._relevance ? -1 : 1) // sort results by relevance
+                }
+                // send the result
+                this.props.setDiseases(searchText == "" ? this.props.state.diseases : searchedItems, (searchText!= "") ? "searched" : false)
+            }.bind(this)), 200)
+        }
+        
+        render() {
+            return (
+                <div className="search">
+                    <input type="text" placeholder="Search the diseases list"onChange={this.onSearchChange}/>
+                    <i className={"fa fa-search"}></i>
+                </div>
+            )            
+        } 
     }
 
     
@@ -26,28 +57,36 @@ try {
         constructor(props) {
             super(props)
             this.state = {
-                content: (
-                    <Loading text="Fetching Diseases..."/>              
-                ),
+                loaderText: "Fetching Diseases...",
+                content: (<span></span>),
+                diseases: [],
                 diseasesLoaded: false
             }
             this.setDiseases = this.setDiseases.bind(this)
-            loadDiseases(this.setDiseases)
+            loadDiseases((d) => this.setDiseases(d, true))
         }
 
-        setDiseases(diseases) {
-            this.setState({content: <div>
-                {
-                    diseases.map(a => 
-                        <div className={"card card-body"} key={Math.random()}>
-                            {Object.keys(a).map(k => <span key={Math.random()}><b>{k}</b>: {a[k]}</span>)}
-                        </div>)
-                }
+        setDiseases(diseases, fetched) {
+            var searchedTx = <span></span>
+            if (fetched == "searched" && diseases.length < this.state.diseases.length) searchedTx = <span className={"grey"}>Filtered <b>{diseases.length}</b> out of <b>{this.state.diseases.length} total items</b></span>
+            this.setState({content: <div>{searchedTx}<div style={{borderRadius: "15px", overflowX: "hidden"}}>
+                    {
+                        diseases.map(a => 
+                            <div className={"card card-body"} key={Math.random()}>
+                                {<div><h3>{a.name}</h3><p className="grey">{a.short_desc}</p></div>}
+                                {/* {Object.keys(a).filter(a => a != "_relevance").map(k => <span key={Math.random()}><b>{k}</b>: {a[k]}</span>)} */}
+                            </div>)
+                    }
+                </div>
             </div>, diseasesLoaded: true})
+            if (fetched === true) this.setState({diseases: diseases})
         }
         
         render() {
-            return <div>{this.state.content}</div>
+            return <div>
+                <SearchDiseases state={this.state} setState={this.setState} setDiseases={this.setDiseases}/>
+                {this.state.diseasesLoaded ? this.state.content : <Loading text={this.state.loaderText}/>}
+                </div>
         }
     }
     
@@ -58,11 +97,10 @@ try {
             <p>Search through the unofficially discovered diseases</p>
         </div>
 
-        <SearchDiseases/>
         <div id="diseases-section">
             <Diseases/>
         </div>
-        <center style={{color: "grey", marginTop: "25px"}}>Version 0.1, by <span style={{color: "black"}}>Thomas Catt</span></center>
+        <center style={{marginTop: "25px"}} className="grey">Version 0.1, by <span>Thomas Catt</span></center>
     </div>, document.getElementById('root'))
 } catch(e) {
     ReactDOM.render(<pre>
--- projects/diseases_explorer/script.js
+++ projects/diseases_explorer/script.js
@@ -7,15 +7,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 try {
-    var SearchDiseases = function SearchDiseases() {
-        return React.createElement(
-            "div",
-            { className: "search" },
-            React.createElement("input", { type: "text", placeholder: "Search the diseases list" }),
-            React.createElement("i", { className: "fa fa-search" })
-        );
-    };
-
     var loadDiseases = function loadDiseases(callback) {
         if (location.href.includes(".github.io")) request("data.json", callback);else {
             var data = testData;
@@ -28,49 +19,147 @@ try {
     var request = window.request;
     var testData = window.testData;
 
-    var Diseases = function (_React$Component) {
-        _inherits(Diseases, _React$Component);
+    var SearchDiseases = function (_React$Component) {
+        _inherits(SearchDiseases, _React$Component);
+
+        function SearchDiseases(props) {
+            _classCallCheck(this, SearchDiseases);
+
+            var _this = _possibleConstructorReturn(this, (SearchDiseases.__proto__ || Object.getPrototypeOf(SearchDiseases)).call(this, props));
+
+            _this.onSearchChange = _this.onSearchChange.bind(_this);
+            return _this;
+        }
+
+        _createClass(SearchDiseases, [{
+            key: "onSearchChange",
+            value: function onSearchChange(e) {
+                clearTimeout(window.searchDelay);
+                window.searchDelay = setTimeout(function () {
+                    var searchText = e.target.value; // the search text
+                    var searchTerms = searchText.split(" ").filter(function (a) {
+                        return a != "";
+                    }); // search text split on spaces to form 'terms' to match
+                    if (searchTerms.length > 0) {
+                        // only continue search if the search terms exist
+                        var searchedItemsRelevance = []; // relevance array, stores relevance for each match, used for sorting later
+                        var searchedItems = this.props.state.diseases.filter(function (a) {
+                            // performs the actual matching
+                            var matches = searchTerms.map(function (t) {
+                                return a.tags.map(function (tag) {
+                                    return tag.includes(t);
+                                }).reduce(function (a, b) {
+                                    return a || b;
+                                });
+                            }); // check for tags of each disease, and run a .includes() for each of the search terms
+                            searchedItemsRelevance.push(matches.filter(function (a) {
+                                return a == true;
+                            }).length); // checks relevance, which is number of search terms found in tags
+                            return matches.reduce(function (a, b) {
+                                return a || b;
+                            });
+                        });
+
+                        for (var i = 0; i < searchedItems.length; i++) {
+                            // attach the separated search relevance to the searchedItems array
+                            searchedItems[i]._relevance = searchedItemsRelevance[i];
+                        }
+
+                        searchedItems = searchedItems.sort(function (a, b) {
+                            return a._relevance > b._relevance ? -1 : 1;
+                        }); // sort results by relevance
+                    }
+                    // send the result
+                    this.props.setDiseases(searchText == "" ? this.props.state.diseases : searchedItems, searchText != "" ? "searched" : false);
+                }.bind(this), 200);
+            }
+        }, {
+            key: "render",
+            value: function render() {
+                return React.createElement(
+                    "div",
+                    { className: "search" },
+                    React.createElement("input", { type: "text", placeholder: "Search the diseases list", onChange: this.onSearchChange }),
+                    React.createElement("i", { className: "fa fa-search" })
+                );
+            }
+        }]);
+
+        return SearchDiseases;
+    }(React.Component);
+
+    var Diseases = function (_React$Component2) {
+        _inherits(Diseases, _React$Component2);
 
         function Diseases(props) {
             _classCallCheck(this, Diseases);
 
-            var _this = _possibleConstructorReturn(this, (Diseases.__proto__ || Object.getPrototypeOf(Diseases)).call(this, props));
+            var _this2 = _possibleConstructorReturn(this, (Diseases.__proto__ || Object.getPrototypeOf(Diseases)).call(this, props));
 
-            _this.state = {
-                content: React.createElement(Loading, { text: "Fetching Diseases..." }),
+            _this2.state = {
+                loaderText: "Fetching Diseases...",
+                content: React.createElement("span", null),
+                diseases: [],
                 diseasesLoaded: false
             };
-            _this.setDiseases = _this.setDiseases.bind(_this);
-            loadDiseases(_this.setDiseases);
-            return _this;
+            _this2.setDiseases = _this2.setDiseases.bind(_this2);
+            loadDiseases(function (d) {
+                return _this2.setDiseases(d, true);
+            });
+            return _this2;
         }
 
         _createClass(Diseases, [{
             key: "setDiseases",
-            value: function setDiseases(diseases) {
+            value: function setDiseases(diseases, fetched) {
+                var searchedTx = React.createElement("span", null);
+                if (fetched == "searched" && diseases.length < this.state.diseases.length) searchedTx = React.createElement(
+                    "span",
+                    { className: "grey" },
+                    "Filtered ",
+                    React.createElement(
+                        "b",
+                        null,
+                        diseases.length
+                    ),
+                    " out of ",
+                    React.createElement(
+                        "b",
+                        null,
+                        this.state.diseases.length,
+                        " total items"
+                    )
+                );
                 this.setState({ content: React.createElement(
                         "div",
                         null,
-                        diseases.map(function (a) {
-                            return React.createElement(
-                                "div",
-                                { className: "card card-body", key: Math.random() },
-                                Object.keys(a).map(function (k) {
-                                    return React.createElement(
-                                        "span",
-                                        { key: Math.random() },
+                        searchedTx,
+                        React.createElement(
+                            "div",
+                            { style: { borderRadius: "15px", overflowX: "hidden" } },
+                            diseases.map(function (a) {
+                                return React.createElement(
+                                    "div",
+                                    { className: "card card-body", key: Math.random() },
+                                    React.createElement(
+                                        "div",
+                                        null,
                                         React.createElement(
-                                            "b",
+                                            "h3",
                                             null,
-                                            k
+                                            a.name
                                         ),
-                                        ": ",
-                                        a[k]
-                                    );
-                                })
-                            );
-                        })
+                                        React.createElement(
+                                            "p",
+                                            { className: "grey" },
+                                            a.short_desc
+                                        )
+                                    )
+                                );
+                            })
+                        )
                     ), diseasesLoaded: true });
+                if (fetched === true) this.setState({ diseases: diseases });
             }
         }, {
             key: "render",
@@ -78,7 +167,8 @@ try {
                 return React.createElement(
                     "div",
                     null,
-                    this.state.content
+                    React.createElement(SearchDiseases, { state: this.state, setState: this.setState, setDiseases: this.setDiseases }),
+                    this.state.diseasesLoaded ? this.state.content : React.createElement(Loading, { text: this.state.loaderText })
                 );
             }
         }]);
@@ -109,7 +199,6 @@ try {
                 "Search through the unofficially discovered diseases"
             )
         ),
-        React.createElement(SearchDiseases, null),
         React.createElement(
             "div",
             { id: "diseases-section" },
@@ -117,11 +206,11 @@ try {
         ),
         React.createElement(
             "center",
-            { style: { color: "grey", marginTop: "25px" } },
+            { style: { marginTop: "25px" }, className: "grey" },
             "Version 0.1, by ",
             React.createElement(
                 "span",
-                { style: { color: "black" } },
+                null,
                 "Thomas Catt"
             )
         )
